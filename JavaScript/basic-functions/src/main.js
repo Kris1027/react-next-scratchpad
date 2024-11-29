401663b78d56e8aeb0796de90d9e77d7b3b197a4
Kris1027 <obarzanek.work@gmail.com>
2024-11-29 20:06:24 +0100

    add math functions

--- JavaScript/basic-functions/src/main.js
+++ JavaScript/basic-functions/src/main.js
@@ -2,8 +2,10 @@
 // import { ObjectFunctions } from './object-functions';
 // import { StringFunctions } from './string-functions';
 // import { DataConversion } from './data-conversion';
+// import { MathFunctions } from './math-functions';
 
 // ArrayFunctions();
 // ObjectFunctions();
 // StringFunctions();
 // DataConversion();
+// MathFunctions();
---
+++ JavaScript/basic-functions/src/math-functions.js
@@ -0,0 +1,14 @@
+export function MathFunctions() {
+    // Generuje losową liczbę z zakresu [0, 1)
+    console.log(Math.random()); // np. 0.873
+
+    // Zaokrągla liczbę w dół
+    console.log(Math.floor(4.7)); // 4
+
+    // Zaokrągla liczbę w górę
+    console.log(Math.ceil(4.2)); // 5
+
+    // Zaokrągla liczbę do najbliższej liczby całkowitej
+    console.log(Math.round(4.5)); // 5
+    console.log(Math.round(4.4)); // 4
+}
